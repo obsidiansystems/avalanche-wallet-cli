6e9eb7d94e598d1296d3cafe4c356f1f1ca75c29
Jacquin Mininger <jacquin.mininger@gmail.com>
2020-08-24 11:50:18 -0400

    nixify avalanche

---
+++ nix/avalanche.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  coreth = (import ./coreth.nix) {};
+in 
+  pkgs.buildGoModule {
+  name = "gecko";
+  src = import ./dep/gecko/thunk.nix;
+  vendorSha256 = "1z6iy389j5an2qsjfjib2hcal0fpqkj49qarcib5vlgsy7skmnzw";
+  # vendorSha256 = pkgs.lib.fakeSha256;
+  doCheck = false;
+  buildPhase = ''
+    mkdir -p $out
+    mkdir -p $out/bin
+    mkdir -p $out/plugins
+    cp ${coreth}/bin/evm $out/plugins/evm
+    go build -mod=vendor -o $out/bin/avalanche "$src/main/"*.go
+  '';
+  dontInstall = true;
+}
+
--- nix/avash.nix
+++ nix/avash.nix
@@ -2,16 +2,18 @@
 pkgs.buildGoModule {
   name = "avash";
   src = import ./dep/avash/thunk.nix;
-  vendorSha256 = "0zhqhzmw9719857ay11l11jh1vmngjkmgbchqijbr28dw2j52wvz";
-  doCheck = false;
-  dontInstall = true;
-  # This import messes up `go mod vendor` as it seems to be unnecessary, so we remove it
+
+  # The import being removed below messes up `go mod vendor` as it seems to be unnecessary
   preConfigure = ''
      substituteInPlace ./wallets/chains/wallet.go --replace '"github.com/ava-labs/gecko/modules/chains/payments"' "" 
     '';
   buildPhase = ''
     mkdir -p $out
     mkdir -p $out/bin
-    go build -mod vendor -o $out/bin/avash main.go
+    go build -mod=vendor -o $out/bin/avash main.go
   '';
+  dontInstall = true;
+  doCheck = false;
+
+  vendorSha256 = "0zhqhzmw9719857ay11l11jh1vmngjkmgbchqijbr28dw2j52wvz";
 }

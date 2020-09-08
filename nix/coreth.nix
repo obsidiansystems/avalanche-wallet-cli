b5cae9dfbda277bb9aa53c6222d5419298d5881e
Jacquin Mininger <jacquin.mininger@gmail.com>
2020-09-08 11:00:32 -0400

    add go-tidy function to patch src before building

--- nix/coreth.nix
+++ nix/coreth.nix
@@ -1,18 +1,49 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.buildGoModule {
-    name = "coreth";
-    src = import ./dep/coreth/thunk.nix;
+{ pkgs ? import <nixpkgs>{}}:
+let
+	fetchThunk = p:
+		if builtins.pathExists (p + /git.json)
+			then pkgs.fetchgit { inherit (builtins.fromJSON (builtins.readFile (p + /git.json))) url rev sha256; }
+		else if builtins.pathExists (p + /github.json)
+			then pkgs.fetchFromGitHub { inherit (builtins.fromJSON (builtins.readFile (p + /github.json))) owner repo rev sha256; }
+		else p;
 
-    # We use the antiquated modSha256 here, instead of the newer vendorSha256 because of this 
-    # go issue: https://github.com/golang/go/issues/26366
-    # This basically states that go mod vendor does not pull in sub directories not containing any go files
-    # This is an issue for libraries like https://github.com/karalabe/usb that contain C deps, and explicitly
-    # reference the path to these dirs in their package.
-    # One solution could be to patch these libs with dummy.go files, another could be to use 
-    # nixpkgs.buildGoModule's support for https://github.com/nomad-software/vend via
-    # `runVend = true;`
-    modSha256 = "1nss6kc3qvli16f36ij0kh52r62f29s5q1qpvbh91f0mvj6n1ylc";
-    vendorSha256 = null;
+	# This function allows us to patch the src with 'go mod tidy' before building
+	runGoModTidy = go: go.stdenv.mkDerivation {
+	  name = "setup";
+    src = fetchThunk ./dep/coreth;
+    nativeBuildInputs = with pkgs; [ go git cacert ];
+    inherit (go) GOOS GOARCH;
+    GO111MODULE = "on";
+
+    impureEnvVars = pkgs.lib.fetchers.proxyImpureEnvVars ++ [
+      "GIT_PROXY_COMMAND" "SOCKS_SERVER"
+    ];
+
+    configurePhase = ''
+      runHook preConfigure
+      export GOCACHE=$TMPDIR/go-cache
+      export GOPATH="$TMPDIR/go"
+      runHook postConfigure
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+			go mod tidy
+			mkdir -p $out
+			mkdir -p $out/patched
+			cp -r ./** $out/patched
+    '';
+		dontInstall = true;
+		outputHashMode = "recursive";
+		outputHashAlgo = "sha256";
+		outputHash = "sha256:00v1k2dafm9g05ains65j1igfkjfmgymv4nhm5fsjx19178dzj9c";
+
+	};
+in pkgs.buildGoModule {
+    name = "coreth";
+    src = "${runGoModTidy pkgs.buildPackages.go_1_15}/patched";
+    vendorSha256 = "sha256:0xdki543lw1h0kd9pnp106w55xaamwfcvd9k47jrmvynr3jdpmmp";
+    runVend = true;
     doCheck = false;
     buildPhase = '' 
       mkdir -p $out

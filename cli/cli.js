9e4e59848e3dc46a4b6b9aedff0fea3875757eff
Elliot Cameron <elliot.cameron@obsidian.systems>
2020-09-09 15:41:49 -0400

    Fix usage of signatures

--- cli/cli.js
+++ cli/cli.js
@@ -445,20 +445,16 @@ async function sign_with_ledger(ledger, txbuff, path_suffixes) {
   const path_suffixes_arr = Array.from(path_suffixes);
   console.error("Signing transaction", txbuff.toString('hex').toUpperCase(), "with paths", path_suffixes_arr);
   requestLedgerAccept();
-  console.error("Raw bytes:");
-  var str = "";
-  txbuff.forEach(x => str += "0x" + (x < 0x10 ? "0" : "") + x.toString(16) + ", ");
-  console.error(str);
   const path_suffix_to_sig = await ledger.signTransaction(
     BipPath.fromString(AVA_BIP32_PREFIX), path_suffixes_arr.map(x => BipPath.fromString(x, false)), txbuff
   ).catch(log_error_and_exit);
 
   console.error("Signatures:");
-  path_suffix_to_sig.forEach((value, key) => {
+  path_suffix_to_sig.signatures.forEach((value, key) => {
     console.error(" ", key + ":", value.toString("hex"));
   });
 
-  return path_suffix_to_sig;
+  return path_suffix_to_sig.signatures;
 }
 
 function parse_amount(str) {
